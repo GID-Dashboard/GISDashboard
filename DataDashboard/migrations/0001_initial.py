c0d21e59cdeb333773b9510db8f4e55f4e1edf9d
James Wright <jdjwright@gmail.com>
2019-12-09 11:17:10 +0800

    fixed several errors due to odd migration problems.

--- DataDashboard/migrations/0001_initial.py
+++ DataDashboard/migrations/0001_initial.py
@@ -52,7 +52,7 @@ class Migration(migrations.Migration):
             ],
         ),
         migrations.CreateModel(
-            name='LocalStudent',
+            name='Student',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('student_id', models.IntegerField(blank=True, null=True)),
@@ -100,11 +100,11 @@ class Migration(migrations.Migration):
             ],
         ),
         migrations.CreateModel(
-            name='LocalTeachingGroup',
+            name='TeachingGroup',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('name', models.CharField(max_length=20)),
-                ('students', models.ManyToManyField(blank=True, to='DataDashboard.LocalStudent')),
+                ('students', models.ManyToManyField(blank=True, to='DataDashboard.Student')),
                 ('teachers', models.ManyToManyField(blank=True, to='DataDashboard.Teacher')),
             ],
         ),
@@ -126,7 +126,7 @@ class Migration(migrations.Migration):
                 ('date', models.DateField()),
                 ('value', models.FloatField()),
                 ('data', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='DataDashboard.SummativeDefinition')),
-                ('student', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='DataDashboard.LocalStudent')),
+                ('student', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='DataDashboard.Student')),
             ],
         ),
         migrations.AddField(
@@ -159,13 +159,13 @@ class Migration(migrations.Migration):
                 ('numerical_value', models.FloatField(blank=True, null=True)),
                 ('text_value', models.TextField(blank=True, null=True)),
                 ('field', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='DataDashboard.MarksheetFields')),
-                ('student', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='DataDashboard.LocalStudent')),
+                ('student', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='DataDashboard.Student')),
             ],
         ),
         migrations.AddField(
             model_name='marksheet',
             name='classgroups',
-            field=models.ManyToManyField(blank=True, to='DataDashboard.LocalTeachingGroup'),
+            field=models.ManyToManyField(blank=True, to='DataDashboard.TeachingGroup'),
         ),
         migrations.AddField(
             model_name='marksheet',
@@ -215,7 +215,7 @@ class Migration(migrations.Migration):
                 ('type', models.CharField(max_length=100)),
                 ('comments', models.TextField(blank=True, null=True)),
                 ('points', models.FloatField(default=0)),
-                ('student', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='DataDashboard.LocalStudent')),
+                ('student', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='DataDashboard.Student')),
                 ('teacher_assigning', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, to='DataDashboard.Teacher')),
             ],
         ),
@@ -241,7 +241,7 @@ class Migration(migrations.Migration):
                 ('pass7', models.IntegerField(blank=True, null=True)),
                 ('pass8', models.IntegerField(blank=True, null=True)),
                 ('pass9', models.IntegerField(blank=True, null=True)),
-                ('student', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='DataDashboard.LocalStudent')),
+                ('student', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='DataDashboard.Student')),
             ],
         ),
         migrations.CreateModel(
@@ -253,7 +253,7 @@ class Migration(migrations.Migration):
                 ('non_verbal', models.IntegerField(blank=True, null=True)),
                 ('quantitative', models.IntegerField(blank=True, null=True)),
                 ('spatial', models.IntegerField(blank=True, null=True)),
-                ('student', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='DataDashboard.LocalStudent')),
+                ('student', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='DataDashboard.Student')),
             ],
         ),
     ]
