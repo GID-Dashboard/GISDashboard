88988bfd2b41b253e4945cb51883f72ce4904d31
James Wright <jdjwright@gmail.com>
2019-12-09 08:17:34 +0800

    Basic strategy advisor created

---
+++ Dashboard/settings/secret_gis.py
@@ -0,0 +1,24 @@
+from .settings import *
+
+DATABASES = {
+     'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'DataDashboard',
+        'USER': 'dashboardadm',
+        'PASSWORD': '@dmDashb0ard',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    },
+    'sims': {
+        'ENGINE': 'sql_server.pyodbc',
+        'NAME': 'gis111207',
+        'HOST': 'GISSIMSSVR.gardenschool.edu.my\sims2008',
+        'USER': 'dashboardadm',
+        'PASSWORD': '@dmDashb0ard',
+
+        'OPTIONS': {
+             'driver': "ODBC Driver 17 for SQL Server",
+             'extra_params': "CurrentSchema=sims",
+         },
+    }
+}
