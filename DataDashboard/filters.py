bfa97f4173f6754e2e5586855b50dc915eb3c442
James Wright <jdjwright@gmail.com>
2019-12-09 10:21:48 +0800

    Tentative student group filtering

--- DataDashboard/filters.py
+++ DataDashboard/filters.py
@@ -6,10 +6,9 @@ class StudentFilter(django_filters.FilterSet):
     last_name = django_filters.CharFilter(lookup_expr='icontains')
     tg_choices = list(Student.objects.all().values_list('tutor_group_id', 'tutor_group_id').distinct().order_by('tutor_group_id'))
     house_choices = list(Student.objects.all().values_list('house_id', 'house_id').distinct().order_by('house_id'))
-    teaching_group_choices = list(LocalTeachingGroup.objects.all().values_list('name', 'name').distinct().order_by('name'))
     tutor_group_id = django_filters.MultipleChoiceFilter(choices=tg_choices)
     house_id = django_filters.MultipleChoiceFilter(choices=house_choices)
-    teachinggroup__name = django_filters.CharFilter(lookup_expr='icontains')
+    teachinggroup__teaching_group = django_filters.CharFilter(lookup_expr='icontains')
 
     class Meta:
         model = Student
